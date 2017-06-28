50939dcff9bbc2d41ee42bfff25e12bc4db408e5
liqiang <837769474@qq.com>
2017-06-28 23:43:39 +0800

    走势图

--- www/js/controllers/DrawCtrl.js
+++ www/js/controllers/DrawCtrl.js
@@ -3,9 +3,9 @@
  */
 //开奖
 angular.module ('starter.DrawCtrl', ['starter.services'])
-    
+
     .controller ('DrawCtrl', function ($scope, $ionicSlideBoxDelegate) {
-        $scope.tabNames = ['大乐透', '排列 3', '排列 5'];
+        /*$scope.tabNames = ['大乐透', '排列 3', '排列 5'];
         $scope.slectIndex = 0;
         $scope.activeSlide = function (index) {//点击时候触发
             $scope.slectIndex = index;
@@ -14,5 +14,7 @@ angular.module ('starter.DrawCtrl', ['starter.services'])
         $scope.slideChanged = function (index) {//滑动时候触发
             $scope.slectIndex = index;
         };
-        $scope.pages = ["templates/bigLottoHistoryDetails.html", "templates/exchangehistory3D.html", "templates/exchangehistory5D.html"];
+        $scope.pages = ["templates/bigLottoHistoryDetails.html", "templates/exchangehistory3D.html", "templates/exchangehistory5D.html"];*/
+
+
     });
