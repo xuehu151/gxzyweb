5303920482ad4ebd7e7802d1501130735698b80f
xushuming <xuniaoji@163.com>
2017-09-06 19:36:11 +0800

    添加url.js

---
+++ www/url.js
@@ -0,0 +1,4 @@
+// var ipUrl = 'http://lottery.zhenlong.wang';
+// var url = 'http://lottery.zhenlong.wang';
+var ipUrl = 'http://103.235.237.134';      //本地ip地址或者域名
+var url = 'http://103.235.237.134';      //本地ip地址或者域名
