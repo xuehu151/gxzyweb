0d0cf24b32944c1e30cce1133ebb9b39cfd245f2
liqiang <173246lq>
2018-01-26 14:24:40 +0800

    增加新需求

--- www/js/app.js
+++ www/js/app.js
@@ -9,11 +9,9 @@ var PayType = '';//判断投注方式   0 扫码投注 1 余额投注
 
 // var ipUrl = 'http://103.235.237.134';      //本地ip地址或者域名
 // var url = 'http://103.235.237.134';      //本地ip地址或者域名
+angular.module ('starter', ['ionic', 'starter.needExchangeCtrl', 'starter.services', 'starter.util', 'starter.ExchangeCtrl', 'starter.scanCodeIndexCtrl', 'starter.Exchange-3Ctrl', 'starter.Exchange-3DetailsCtrl', 'starter.Exchangehistory3DCtrl', 'starter.Exchange-5Ctrl', 'starter.Exchange-5DetailsCtrl', 'starter.Exchangehistory5DCtrl', 'starter.BigLotto-2Ctrl', 'starter.bettingDetailCtrl', 'starter.bigLottoHistoryDetailsCtrl', 'starter.AccountCtrl', 'starter.completeInfoCtrl', 'starter.completeInfoSucceedCtrl', 'starter.widthdrawResultCtrl', 'starter.prizeRecordsCtrl', 'starter.allOrdersCtrl', 'starter.orderDetailCtrl',  'starter.widthdrawCtrl', 'starter.DrawCtrl', 'starter.TrendCtrl', 'starter.3DTrendChart', 'starter.5DTrendChart', 'starter.bigTrendChart', 'starter.selfInfoCtrl', 'starter.appSotreCtrl'])
 
-
-angular.module ('starter', ['ionic', 'starter.needExchangeCtrl', 'starter.services', 'starter.util', 'starter.ExchangeCtrl', 'starter.scanCodeIndexCtrl', 'starter.Exchange-3Ctrl', 'starter.Exchange-3DetailsCtrl', 'starter.Exchangehistory3DCtrl', 'starter.Exchange-5Ctrl', 'starter.Exchange-5DetailsCtrl', 'starter.Exchangehistory5DCtrl', 'starter.BigLotto-2Ctrl', 'starter.bettingDetailCtrl', 'starter.bigLottoHistoryDetailsCtrl', 'starter.AccountCtrl', 'starter.completeInfoCtrl', 'starter.completeInfoSucceedCtrl', 'starter.widthdrawResultCtrl', 'starter.prizeRecordsCtrl', 'starter.allOrdersCtrl', 'starter.orderDetailCtrl',  'starter.widthdrawCtrl', 'starter.DrawCtrl', 'starter.TrendCtrl', 'starter.3DTrendChart', 'starter.5DTrendChart', 'starter.bigTrendChart', 'starter.selfInfoCtrl'])
-
-    .run (function ($ionicPlatform,$ionicPopup,$rootScope, $ionicHistory) {
+    .run (function ($ionicPlatform) {
         $ionicPlatform.ready (function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
@@ -28,36 +26,6 @@ angular.module ('starter', ['ionic', 'starter.needExchangeCtrl', 'starter.servic
             ionic.Platform.fullScreen(true, true);
         });
 
-        $ionicPlatform.registerBackButtonAction (function (e) {
-            e.preventDefault ();
-            function showConfirm () {
-                var confirmPopup = $ionicPopup.confirm ({
-                    title : '<strong>退出应用?</strong>',
-                    template : '你确定要退出应用吗?',
-                    okText : '退出',
-                    cancelText : '取消'
-                });
-                confirmPopup.then (function (res) {
-                    if (res) {
-                        ionic.Platform.exitApp ();
-                    }
-                    else {
-                    //..........
-                    }
-                });
-            }
-
-            if ($location.path () == '/home/homeInfo') {
-                showConfirm ();
-            }
-            else if ($ionicHistory.backView ()) {
-                $ionicHistory.goBack ();
-            }
-            else {
-                showConfirm ();
-            }
-            return false;
-        }, 101);
     })
 
     .config (function ($stateProvider, $urlRouterProvider, $ionicConfigProvider,$httpProvider, $locationProvider) {
@@ -85,9 +53,6 @@ angular.module ('starter', ['ionic', 'starter.needExchangeCtrl', 'starter.servic
                 }
             }
         })*/
-
-
-
         //解决tabs在Android顶部的方法
         $ionicConfigProvider.platform.ios.tabs.style('standard');
         $ionicConfigProvider.platform.ios.tabs.position('bottom');
@@ -119,7 +84,18 @@ angular.module ('starter', ['ionic', 'starter.needExchangeCtrl', 'starter.servic
                 url: '/tab',
                 abstract: true,
                 templateUrl: 'templates/tabs.html',
-                controller:'tabsCtrl'
+                // controller:'tabCtrl'
+            })
+
+            //app下载
+            .state ('tab.appSotre', {
+                url: '/appSotre',
+                views: {
+                    'tab-appSotre': {
+                        templateUrl: 'templates/appSotre.html',
+                        controller: 'appSotreCtrl'
+                    }
+                }
             })
 
             //兑换
---
+++ www/js/controllers/appSotreCtrl.js
@@ -0,0 +1,48 @@
+/**
+ * Created by admin on 2018/1/25.
+ */
+angular.module ('starter.appSotreCtrl', [])
+
+    .controller ('appSotreCtrl', function ($scope, $ionicModal) {
+        $scope.oncedownloading = function () {
+            var ua = navigator.userAgent.toLowerCase ();
+            var isWeixin = ua.indexOf ('micromessenger') != -1;
+            if (isWeixin) {
+                $scope.modalTips.show ();
+                window.addEventListener ("popstate", function (e) {
+                    if ($scope.modalTips.show ()) {
+                        $scope.modalTips.hide ();
+                    }
+                }, false);
+            }
+            else {
+                window.location.href = 'http://s.baibaocp.com/app/bbcp_gouli_v2.apk';
+            }
+        };
+        $scope.iPhoneOpen = function () {
+            var ua = navigator.userAgent.toLowerCase ();
+            var isWeixin = ua.indexOf ('micromessenger') != -1;
+            if (isWeixin) {
+                $scope.modalTips.show ();
+                window.addEventListener ("popstate", function (e) {
+                    if ($scope.modalTips.show ()) {
+                        $scope.modalTips.hide ();
+                    }
+                }, false);
+            }
+            else {
+                var phoneUrl = 'http://m.baibaocp.com/index2.php?qd=1001';
+                window.open (phoneUrl, '_blank');
+            }
+        };
+
+        //错误码窗口配置
+        $ionicModal.fromTemplateUrl ('templates/tips.html', {
+            scope : $scope,
+            backdropClickToClose : true
+        })
+            .then (function (modal) {
+                $scope.modalTips = modal;
+            });
+
+    });
