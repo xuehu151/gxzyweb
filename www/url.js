1ceb20c620c54f31432cd5782aa944b09873d1bb
liqiang <173246lq>
2018-04-09 16:49:46 +0800

    更换二维码

--- www/url.js
+++ www/url.js
@@ -1,4 +1,11 @@
 // var ipUrl = 'http://lottery.zhenlong.wang';
 // var url = 'http://lottery.zhenlong.wang';
-var ipUrl = 'http://103.235.237.134';      //本地ip地址或者域名
-var url = 'http://103.235.237.134';      //本地ip地址或者域名
+// var ipUrl = 'http://103.235.237.134';      //本地ip地址或者域名
+// var url = 'http://103.235.237.134';      //本地ip地址或者域名
+
+
+var ipUrl = 'http://47.104.29.94:8090';      //本地ip地址或者域名
+var url = 'http://47.104.29.94:8090';      //本地ip地址或者域名
+
+
+
